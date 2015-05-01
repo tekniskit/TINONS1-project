a672170e663fb3cc2edbc3eed15e7215c16f5195
Rasmus Reimer <rasmus.reimer@gmail.com>
2015-05-01 11:03:17 +0200

    project structure

--- Software/main.m
+++ Software/main.m
@@ -3,11 +3,12 @@
 
 clear, close all, clc;
 
-addpath('.\vb');
+addpath('libs/vb');
+addpath('dataProcessing');
 
-[ training1, test1, Fs1 ] = readDataFromFile('glud1.mp3');
-[ training2, test2, Fs2 ] = readDataFromFile('Reimer1.mp3');
-[ training3, test3, Fs3 ] = readDataFromFile('Rune1.mp3');
+[ training1, test1, Fs1 ] = readDataFromFile('data/glud1.mp3');
+[ training2, test2, Fs2 ] = readDataFromFile('data/Reimer1.mp3');
+[ training3, test3, Fs3 ] = readDataFromFile('data/Rune1.mp3');
 
 features1 = extractFeatures(training1, Fs1);
 features2 = extractFeatures(training2, Fs2);
